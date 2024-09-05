b3b8a187fae30acca7d12b1cd8d09c1e6e4ba3e5
WillTheWater <willthewater@protonmail.com>
2024-09-05 16:59:22 +0200

    Project Setup

---
+++ CoopProjectV3/src/Game.cpp
@@ -0,0 +1,22 @@
+#include "Game.h"
+
+void Game::Run()
+{
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Default Window");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
